c7d17cbcf16ce2924fb2c54bb943ceba31d7c8c0
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-07-29 15:20:27 +0400

    added displayName to user object

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,6 +4,7 @@ import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 import { app } from "./firebase";
 import store from "@/store";
@@ -13,13 +14,15 @@ const auth = getAuth(app);
 export function initAuthState() {
   onAuthStateChanged(auth, (user) => {
     store.commit("setUser", user);
+    console.log(user);
     store.commit("setAuthState", true);
   });
 }
 
-export async function signUp(email, password) {
+export async function signUp(name, email, password) {
   const res = await createUserWithEmailAndPassword(auth, email, password);
   if (res) {
+    await updateProfile(auth.currentUser, { displayName: name });
     store.commit("setUser", res.user);
   } else {
     throw new Error("sign up function failed!");

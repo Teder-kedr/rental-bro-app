8d030441c50ff1ae2080f0ed386378e6dcbab754
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-07-29 15:07:30 +0400

    styled login page

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,10 +1,46 @@
 <template>
-  <form @submit.prevent="handleLogIn">
-    <input type="text" placeholder="Email" v-model="email" />
-    <input type="password" placeholder="Password" v-model="password" />
-    <button type="submit">Log In</button>
-  </form>
+  <div class="h-screen d-flex justify-center align-center">
+    <v-card class="mx-auto mb-4 px-6 py-8" width="350">
+      <h2 class="mb-4">Log In</h2>
+      <v-form @submit.prevent="handleLogIn">
+        <v-text-field
+          required
+          :readonly="isLoading"
+          label="Email"
+          type="email"
+          v-model="email"
+        ></v-text-field>
+        <v-text-field
+          required
+          :readonly="isLoading"
+          label="Password"
+          type="password"
+          v-model="password"
+        ></v-text-field>
+        <v-btn
+          color="primary"
+          block
+          :disabled="!form"
+          :loading="isLoading"
+          type="submit"
+          >Log in</v-btn
+        >
+      </v-form>
+
+      <v-divider class="my-4"></v-divider>
+
+      <p class="text-center hint-text">
+        Don't have an account? <router-link to="signup">Sign up!</router-link>
+      </p>
+    </v-card>
+  </div>
 </template>
+
+<style scoped>
+.hint-text {
+  font-size: 0.9rem;
+}
+</style>
 
 <script>
 import { logIn } from "@/services/auth";
@@ -12,20 +48,29 @@ import { logIn } from "@/services/auth";
 export default {
   data() {
     return {
-      email: "",
-      password: "",
+      email: null,
+      password: null,
+      isLoading: false,
     };
   },
   methods: {
     async handleLogIn() {
       try {
+        this.isLoading = true;
         const email = this.email;
         const password = this.password;
         await logIn(email, password);
         this.$router.push("projects");
       } catch (error) {
+        this.isLoading = false;
         console.error("👎🏻", error.message);
       }
+    },
+  },
+  computed: {
+    form() {
+      if (this.email && this.password) return true;
+      return false;
     },
   },
 };

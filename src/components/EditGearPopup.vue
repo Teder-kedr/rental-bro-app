42e0915dd6b09da49ed557eea6b1e5c2b18c84d9
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-08-04 00:15:16 +0400

    EditGearPopup small style change

--- src/components/EditGearPopup.vue
+++ src/components/EditGearPopup.vue
@@ -1,123 +1,125 @@
 <template>
   <v-dialog fullscreen transition="dialog-bottom-transition" :scrim="false">
     <v-card class="pa-8">
-      <v-btn
-        variant="text"
-        position="absolute"
-        icon="mdi-close"
-        class="my-close-button"
-        @click="handleCancel"
-      />
-      <v-card-title class="card-title">Edit item</v-card-title>
-      <v-card-subtitle>id: {{ item.id }}</v-card-subtitle>
-      <v-card-text class="pa-4">
-        <v-row no-gutters>
-          <v-col>
-            <p class="mb-1">Model:</p>
-            <v-text-field
-              v-model="changedItem.model"
-              spellcheck="false"
-              density="compact"
-              variant="outlined"
-            />
-          </v-col>
-        </v-row>
-        <v-row no-gutters>
-          <v-col>
-            <p class="mb-1">Type:</p>
-            <v-text-field
-              v-model="changedItem.type"
-              hide-details
-              density="compact"
-              variant="outlined"
-            />
-            <div class="mt-1 mb-3 d-flex flex-wrap">
-              <v-chip
-                v-for="opt of types"
-                :key="opt"
-                size="small"
-                class="ma-1"
-                @click="changedItem.type = opt"
+      <div style="max-width: 1000px" class="mx-auto">
+        <v-btn
+          variant="text"
+          position="absolute"
+          icon="mdi-close"
+          class="my-close-button"
+          @click="handleCancel"
+        />
+        <v-card-title class="card-title">Edit item</v-card-title>
+        <v-card-subtitle>id: {{ item.id }}</v-card-subtitle>
+        <v-card-text class="pa-4">
+          <v-row no-gutters>
+            <v-col>
+              <p class="mb-1">Model:</p>
+              <v-text-field
+                v-model="changedItem.model"
+                spellcheck="false"
+                density="compact"
+                variant="outlined"
+              />
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col>
+              <p class="mb-1">Type:</p>
+              <v-text-field
+                v-model="changedItem.type"
+                hide-details
+                density="compact"
+                variant="outlined"
+              />
+              <div class="mt-1 mb-3 d-flex flex-wrap">
+                <v-chip
+                  v-for="opt of types"
+                  :key="opt"
+                  size="small"
+                  class="ma-1"
+                  @click="changedItem.type = opt"
+                >
+                  {{ opt }}
+                </v-chip>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col cols="12" sm="6">
+              <p class="mb-1">Price per day (₽):</p>
+              <v-text-field
+                v-model="changedItem.priceday"
+                class="hide-spinners me-sm-2"
+                type="number"
+                @keydown.up.prevent
+                @keydown.down.prevent
+                density="compact"
+                variant="outlined"
+              />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <p class="mb-1">Quantity:</p>
+              <v-text-field
+                v-model="changedItem.qty"
+                class="hide-spinners ms-sm-2"
+                type="number"
+                @keydown.up.prevent
+                @keydown.down.prevent
+                density="compact"
+                variant="outlined"
               >
-                {{ opt }}
-              </v-chip>
-            </div>
-          </v-col>
-        </v-row>
-        <v-row no-gutters>
-          <v-col cols="12" sm="6">
-            <p class="mb-1">Price per day (₽):</p>
-            <v-text-field
-              v-model="changedItem.priceday"
-              class="hide-spinners me-sm-2"
-              type="number"
-              @keydown.up.prevent
-              @keydown.down.prevent
-              density="compact"
-              variant="outlined"
-            />
-          </v-col>
-          <v-col cols="12" sm="6">
-            <p class="mb-1">Quantity:</p>
-            <v-text-field
-              v-model="changedItem.qty"
-              class="hide-spinners ms-sm-2"
-              type="number"
-              @keydown.up.prevent
-              @keydown.down.prevent
-              density="compact"
-              variant="outlined"
-            >
-              <template v-slot:prepend>
-                <v-btn
-                  variant="text"
-                  icon="mdi-minus"
-                  @click="decrementQty"
-                  @dblclick.prevent
-                ></v-btn>
-              </template>
-              <template v-slot:append>
-                <v-btn
-                  variant="text"
-                  icon="mdi-plus"
-                  @click="incrementQty"
-                  @dblclick.prevent
-                ></v-btn>
-              </template>
-            </v-text-field>
-          </v-col>
-        </v-row>
-      </v-card-text>
-      <v-card-actions class="py-0 d-flex flex-wrap">
-        <v-btn
-          variant="outlined"
-          :block="isScreenSmall"
-          class="ma-1 me-auto px-3"
-          color="error"
-          append-icon="mdi-close"
-          @click="handleDelete"
-        >
-          Delete item
-        </v-btn>
-        <v-btn
-          variant="outlined"
-          :block="isScreenSmall"
-          class="ma-1 px-3"
-          @click="handleCancel"
-        >
-          Cancel
-        </v-btn>
-        <v-btn
-          variant="outlined"
-          :block="isScreenSmall"
-          class="ma-1 px-3"
-          color="success"
-          @click="handleCancel"
-          append-icon="mdi-check"
-        >
-          Save changes
-        </v-btn>
-      </v-card-actions>
+                <template v-slot:prepend>
+                  <v-btn
+                    variant="text"
+                    icon="mdi-minus"
+                    @click="decrementQty"
+                    @dblclick.prevent
+                  ></v-btn>
+                </template>
+                <template v-slot:append>
+                  <v-btn
+                    variant="text"
+                    icon="mdi-plus"
+                    @click="incrementQty"
+                    @dblclick.prevent
+                  ></v-btn>
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="py-0 d-flex flex-wrap">
+          <v-btn
+            variant="outlined"
+            :block="isScreenSmall"
+            class="ma-1 me-auto px-3"
+            color="error"
+            append-icon="mdi-close"
+            @click="handleDelete"
+          >
+            Delete item
+          </v-btn>
+          <v-btn
+            variant="outlined"
+            :block="isScreenSmall"
+            class="ma-1 px-3"
+            @click="handleCancel"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            variant="outlined"
+            :block="isScreenSmall"
+            class="ma-1 px-3"
+            color="success"
+            @click="handleCancel"
+            append-icon="mdi-check"
+          >
+            Save changes
+          </v-btn>
+        </v-card-actions>
+      </div>
     </v-card>
     <v-dialog theme="dark" v-model="isConfirmationOpen">
       <v-card theme="light" class="mx-auto" elevation="10" max-width="600">

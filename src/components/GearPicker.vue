d2004ff8b5fb22eebc0777f1e4ee458fbf29af56
Teder-kedr <124440529+Teder-kedr@users.noreply.github.com>
2023-08-17 18:42:37 +0400

    added table of selected gear

--- src/components/GearPicker.vue
+++ src/components/GearPicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog theme="dark">
+  <v-dialog theme="dark" persistent :model-value="modelValue">
     <v-card
       theme="light"
       color="background"
@@ -10,13 +10,13 @@
       max-width="750px"
     >
       <v-card-title class="pb-0">Gear</v-card-title>
-      <v-card-subtitle class="pb-2" style="border-bottom: 1px solid #ddd"
-        >Picked 0 items</v-card-subtitle
-      >
+      <v-card-subtitle class="pb-2" style="border-bottom: 1px solid #ddd">
+        Picked items: {{ pickedItemsCount }}
+      </v-card-subtitle>
 
-      <v-list>
+      <v-list v-if="isLoaded">
         <v-container class="pa-0">
-          <v-list-item v-for="(item, idx) of items" :key="item.id">
+          <v-list-item v-for="item of items" :key="item.id" @click.prevent>
             <v-row no-gutters>
               <v-col cols="12" sm="8" class="d-flex flex-column justify-center">
                 <v-list-item-title>
@@ -32,9 +32,23 @@
                 sm="4"
                 class="my-2 d-flex justify-end align-center"
               >
-                <v-btn icon="mdi-minus" flat size="small"></v-btn>
-                <p style="width: 2.5rem; text-align: center">{{ idx }}</p>
-                <v-btn icon="mdi-plus" flat size="small"></v-btn>
+                <v-btn
+                  icon="mdi-minus"
+                  flat
+                  size="small"
+                  @click="handleMinus(item)"
+                  @dblclick.prevent
+                />
+                <p style="width: 2.5rem; text-align: center">
+                  {{ item.qtyPicked || 0 }}
+                </p>
+                <v-btn
+                  icon="mdi-plus"
+                  flat
+                  size="small"
+                  @click="handlePlus(item)"
+                  @dblclick.prevent
+                />
               </v-col>
             </v-row>
 
@@ -42,10 +56,11 @@
           </v-list-item>
         </v-container>
       </v-list>
+      <ContentLoader v-else class="mb-4 py-16" />
 
       <v-card-actions style="border-top: 1px solid #ddd">
-        <v-btn>Cancel</v-btn>
-        <v-btn class="ms-auto">Confirm</v-btn>
+        <v-btn @click="handleCancel">Cancel</v-btn>
+        <v-btn class="ms-auto" @click="handleConfirm">Confirm</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -53,15 +68,74 @@
 
 <script>
 import { getGearList } from "@/services/firestore";
+import ContentLoader from "./ContentLoader.vue";
 
 export default {
+  components: { ContentLoader },
+  props: ["gearList", "modelValue"],
   data() {
     return {
       items: [],
+      isLoaded: false,
     };
   },
-  async mounted() {
-    this.items = await getGearList();
+  computed: {
+    pickedItemsCount() {
+      let count = 0;
+      this.items.forEach((item) => {
+        if (item.qtyPicked && item.qtyPicked > 0) {
+          count++;
+        }
+      });
+      return count;
+    },
+  },
+  watch: {
+    async modelValue(newValue) {
+      if (newValue === true) {
+        this.isLoaded = false;
+        this.items = await getGearList();
+        this.isLoaded = true;
+        this.gearList.forEach((pickedItem) => {
+          const theItem = this.items.find((item) => item.id === pickedItem.id);
+          theItem.qtyPicked = pickedItem.qty;
+        });
+      }
+    },
+  },
+  methods: {
+    handlePlus(item) {
+      if (!item.qtyPicked) {
+        item.qtyPicked = 0;
+      }
+      item.qtyPicked++;
+    },
+    handleMinus(item) {
+      if (!item.qtyPicked || item.qtyPicked === 0) return;
+      item.qtyPicked--;
+    },
+    handleCancel() {
+      this.$emit("update:modelValue", false);
+      setTimeout(() => {
+        this.items.forEach((item) => (item.qtyPicked = 0));
+      }, 300);
+    },
+    handleConfirm() {
+      const itemsPicked = [];
+      this.items.forEach((item) => {
+        if (item.qtyPicked > 0) {
+          itemsPicked.push({
+            model: item.model,
+            type: item.type,
+            priceday: item.priceday,
+            id: item.id,
+            qty: item.qtyPicked,
+          });
+        }
+      });
+      this.$emit("change", itemsPicked);
+      this.$emit("update:modelValue", false);
+    },
   },
 };
 </script>
--- src/components/projects/form/StepThree.vue
+++ src/components/projects/form/StepThree.vue
@@ -1,10 +1,36 @@
 <template>
   <div>
     <p class="my-label">Gear:</p>
-    <v-btn block flat prepend-icon="mdi-plus" @click="isGearPickerOpen = true">
+
+    <v-table v-if="gearList.length" density="compact" class="my-table">
+      <thead>
+        <tr>
+          <th class="px-0">Item</th>
+          <th class="text-right">Qty</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item of gearList" :key="item.id">
+          <td class="px-0">{{ item.model }}</td>
+          <td class="text-right">{{ item.qty }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+
+    <v-btn
+      block
+      flat
+      prepend-icon="mdi-plus"
+      class="mt-2"
+      @click="isGearPickerOpen = true"
+    >
       Add gear from my inventory
     </v-btn>
-    <GearPicker v-model="isGearPickerOpen" />
+    <GearPicker
+      v-model="isGearPickerOpen"
+      :gear-list="gearList"
+      @change="handleUpdateGearList"
+    />
     <p class="my-label mt-4">Extra gear and expenses:</p>
   </div>
 </template>
@@ -14,10 +40,25 @@ import GearPicker from "@/components/GearPicker.vue";
 
 export default {
   components: { GearPicker },
+  props: ["gearList"],
   data() {
     return {
       isGearPickerOpen: false,
     };
   },
+  methods: {
+    handleUpdateGearList(newList) {
+      this.$emit("update:gearList", newList);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.my-table {
+  background-color: transparent;
+  max-width: 500px;
+  margin-bottom: 1rem;
+  font-size: 0.9rem;
+}
+</style>
